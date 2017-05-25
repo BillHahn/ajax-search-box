c59b1ef4a02d1bc2a21a39e9ad8db1aaaac986d9
Bill Hahn <BillHahnG@gmail.com>
2017-05-25 14:38:25 -0500

    Fixed index.html > remote: '../search?key=%QUERY' < (BH)

--- Server.js
+++ Server.js
@@ -7,33 +7,17 @@ var mysql = require('mysql');
 const app = express();
 app.enable('trust proxy');
 
-//Auth for MySQL Access via GoogleAPI
-//I Think the following is for remote apps, but not sure.
-/*
-	google.auth.getApplicationDefault(function(err, authClient) {
-		if (err) {
-    	  //return cb(err);
-    }});
-*/
-
-//Simple style not using environment variables in local, cmdline or app.yaml 
-/*
-var connection = mysql.createConnection({
-	host: '35.184.135.20',
-	user: 'root',
-	password: 'gcp2@cloud',
-	database: 'prodcat'
-	
-});
-connection.connect();
-*/
-
 //Get MySQL connection info from environment variables
 var config = {
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE
 };
+
+//Check environment variables to debug accurate
+/*
+console.log("Environment Variables: " + process.env.MYSQL_USER + " / " +
+  process.env.MYSQL_PASSWORD + " / " + process.env.MYSQL_DATABASE); */
 
 if (process.env.INSTANCE_CONNECTION_NAME) {
   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
@@ -47,29 +31,30 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) {
-	res.render('index.html');
+  res.render('index.html');
 });
 
 app.get('/search', function(req, res) {
-	connection.query('SELECT name from products where name like "%' + req.query
-		.key + '%"',
-		function(err, rows, fields) {
-			if (err) throw err;
-			var data = [];
-			var i = 0;
-			for (i = 0; i < rows.length; i++) {
-				data.push(rows[i].name);
-			}
-			res.end(JSON.stringify(data));
-		});
+  connection.query('SELECT name from products where name like "%' + req.query
+    .key + '%"',
+    function(err, rows, fields) {
+      if (err) throw err;
+      var data = [];
+      var i = 0;
+      for (i = 0; i < rows.length; i++) {
+        data.push(rows[i].name);
+      }
+      res.end(JSON.stringify(data));
+    });
 });
 
-// GCP version of Start Server
+// GCP and localhost compatible multi-purpose way to start Server
 const server = app.listen(process.env.PORT || 8081, () => {
-			const port = server.address().port;
-			console.log(`App listening on port ${port}`);
+  const port = server.address().port;
+  console.log(`App listening on port ${port}`);
 }); // >>> To start your app (1) Deploy it: "gcloud app deploy" then (2) "gcloud app browse'
-//Non-GCP way of starting server locally on localhost
+
+//A simpler more hard coded way of starting server localhost
 /*
 var server = app.listen(3000, function() {
 	console.log("We have started our server on port 3000");
